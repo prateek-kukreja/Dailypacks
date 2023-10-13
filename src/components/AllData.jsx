2dfba3d13f65d78c381fe2217c365ab9e88539bb
Prateek <prateekkukreja@outlook.com>
2023-10-13 18:54:51 +0530

    added items to array

---
+++ src/components/AllData.jsx
@@ -0,0 +1,67 @@
+import SpaceBlueBaskMessengerBag from "../images/backpacks/messengerBag/Space-Blue-Bask-Messenger-Bag.png";
+import TerracottaRedBaskMessengerBag from "../images/backpacks/messengerBag/Terracotta-Red-Bask-Messenger-Bag.png";
+import WalnutBrownBaskMessengerBag from "../images/backpacks/messengerBag/Walnut-Brown-Bask-Messenger-Bag.png";
+import NaturalWhiteMustardTrailWeekender from "../images/backpacks/trailWeekender/Natural-White-Mustard-Trail-Weekender.png";
+import NaturalWhiteOliveTrailWeekender from "../images/backpacks/trailWeekender/Natural-White-Olive-Trail-Weekender.png";
+import BlueIdyllTote from "../images/backpacks/toteBag/Blue-Idyll-Tote.png";
+import GreenIdyllTote from "../images/backpacks/toteBag/Green-Idyll-Tote.png";
+import WhiteIdyllTote from "../images/backpacks/toteBag/White-Idyll-Tote.png";
+
+export const items = [
+  {
+    id: 1,
+    category: "baskMessengerBag",
+    img: SpaceBlueBaskMessengerBag,
+    name: "Space Blue Messenger Bag",
+    price: 4499,
+  },
+  {
+    id: 2,
+    category: "baskMessengerBag",
+    img: TerracottaRedBaskMessengerBag,
+    name: "Terracotta Messenger Bag",
+    price: 4999,
+  },
+  {
+    id: 3,
+    category: "baskMessengerBag",
+    img: WalnutBrownBaskMessengerBag,
+    name: "Walnut Brown Messenger Bag",
+    price: 4499,
+  },
+  {
+    id: 4,
+    category: "trailWeekender",
+    img: NaturalWhiteMustardTrailWeekender,
+    name: "White Mustard Trail Weekender",
+    price: 6499,
+  },
+  {
+    id: 5,
+    category: "trailWeekender",
+    img: NaturalWhiteOliveTrailWeekender,
+    name: "White Olive Trail Weekender",
+    price: 5999,
+  },
+  {
+    id: 6,
+    category: "idyllTote",
+    img: GreenIdyllTote,
+    name: "Green Idyll Tote",
+    price: 3999,
+  },
+  {
+    id: 7,
+    category: "idyllTote",
+    img: WhiteIdyllTote,
+    name: "White Idyll Tote",
+    price: 3499,
+  },
+  {
+    id: 8,
+    category: "idyllTote",
+    img: BlueIdyllTote,
+    name: "Blue Idyll Tote",
+    price: 3999,
+  },
+];
