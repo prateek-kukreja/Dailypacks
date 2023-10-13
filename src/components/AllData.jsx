97d46e0ffb17704b218b125acf230b67888fe867
Prateek <prateekkukreja@outlook.com>
2023-10-14 03:32:12 +0530

    small fixes, moved more items in array

--- src/components/AllData.jsx
+++ src/components/AllData.jsx
@@ -6,25 +6,36 @@ import NaturalWhiteOliveTrailWeekender from "../images/backpacks/trailWeekender/
 import BlueIdyllTote from "../images/backpacks/toteBag/Blue-Idyll-Tote.png";
 import GreenIdyllTote from "../images/backpacks/toteBag/Green-Idyll-Tote.png";
 import WhiteIdyllTote from "../images/backpacks/toteBag/White-Idyll-Tote.png";
+import KhakiBeigeOdysseyBackpack from "../images/backpacks/odysseyBackpack/Khaki-Beige-Odyssey-Backpack.png";
+import MintGreenOdysseyBackpack from "../images/backpacks/odysseyBackpack/Mint-Green-Odyssey-Backpack.png";
+import TangerineWayBackpack from "../images/backpacks/wayBackpack/Tangerine-Way-Backpack.png";
+import PineWayBackpack from "../images/backpacks/wayBackpack/Pine-Way-Backpack.png";
+import SlateGrayWayBackpack from "../images/backpacks/wayBackpack/Slate-Gray-Way-Backpack.png";
+import SpaceBlueWayBackpack from "../images/backpacks/wayBackpack/Space-Blue-Way-Backpack.png";
+import BlueScootBackpack from "../images/backpacks/scootBackpack/Blue-Scoot-Backpack.png";
+import GreenScootBackpack from "../images/backpacks/scootBackpack/Green-Scoot-Backpack.png";
+import OrangeScootBackpack from "../images/backpacks/scootBackpack/Orange-Scoot-Backpack.png";
+import RedScootBackpack from "../images/backpacks/scootBackpack/Red-Scoot-Backpack.png";
+import YellowScootBackpack from "../images/backpacks/scootBackpack/Yellow-Scoot-Backpack.png";
 
 export const items = [
   {
     id: 1,
-    category: "baskMessengerBag",
+    category: "messengerBag",
     img: SpaceBlueBaskMessengerBag,
     name: "Space Blue Messenger Bag",
     price: 4499,
   },
   {
     id: 2,
-    category: "baskMessengerBag",
+    category: "messengerBag",
     img: TerracottaRedBaskMessengerBag,
     name: "Terracotta Messenger Bag",
     price: 4999,
   },
   {
     id: 3,
-    category: "baskMessengerBag",
+    category: "messengerBag",
     img: WalnutBrownBaskMessengerBag,
     name: "Walnut Brown Messenger Bag",
     price: 4499,
@@ -45,7 +56,7 @@ export const items = [
   },
   {
     id: 6,
-    category: "idyllTote",
+    category: "totebag",
     img: GreenIdyllTote,
     name: "Green Idyll Tote",
     price: 3999,
@@ -63,5 +74,83 @@ export const items = [
     img: BlueIdyllTote,
     name: "Blue Idyll Tote",
     price: 3999,
+  },
+  {
+    id: 9,
+    category: "odysseyBackpack",
+    img: KhakiBeigeOdysseyBackpack,
+    name: "Khaki Odyssey Backpack",
+    price: 4999,
+  },
+  {
+    id: 10,
+    category: "odysseyBackpack",
+    img: MintGreenOdysseyBackpack,
+    name: "Mint Odyssey Backpack",
+    price: 4999,
+  },
+  {
+    id: 14,
+    category: "scootBackpack",
+    img: RedScootBackpack,
+    name: "Red Scoot Backpack",
+    price: 1199,
+  },
+  {
+    id: 15,
+    category: "scootBackpack",
+    img: YellowScootBackpack,
+    name: "Yellow Scoot Backpack",
+    price: 1199,
+  },
+  {
+    id: 11,
+    category: "scootBackpack",
+    img: GreenScootBackpack,
+    name: "Green Scoot Backpack",
+    price: 1199,
+  },
+
+  {
+    id: 12,
+    category: "scootBackpack",
+    img: BlueScootBackpack,
+    name: "Blue Scoot Backpack",
+    price: 1199,
+  },
+  {
+    id: 13,
+    category: "scootBackpack",
+    img: OrangeScootBackpack,
+    name: "Orange Scoot Backpack",
+    price: 1199,
+  },
+  {
+    id: 16,
+    category: "wayBackpack",
+    img: TangerineWayBackpack,
+    name: "Tangerine Way Backpack",
+    price: 2499,
+  },
+  {
+    id: 17,
+    category: "wayBackpack",
+    img: PineWayBackpack,
+    name: "Pine Way Backpack",
+    price: 2499,
+  },
+  {
+    id: 18,
+    category: "wayBackpack",
+    img: SlateGrayWayBackpack,
+    name: "Slate Gray Way Backpack",
+    price: 2499,
+  },
+  {
+    id: 19,
+    category: "wayBackpack",
+    img: SpaceBlueWayBackpack,
+    name: "Space Blue Way Backpack",
+    price: 2499,
   },
 ];
