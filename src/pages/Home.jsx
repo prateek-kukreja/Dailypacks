b9a618b0455bd40a9f77553451bd530bcdc01b83
Prateek <prateekkukreja@outlook.com>
2023-10-14 03:03:18 +0530

    Banner added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
+import Banner from "../components/Banner";
 import Hero from "../components/Hero";
 import ProudProducts from "../components/ProudProducts";
+import Banner1 from "../images/backpacks/banner/scoot-backpack.png";
 
 function Home() {
   return (
     <>
       <Hero />
       <ProudProducts />
+      <Banner
+        title="SCOOT BACKPACK"
+        text="A trendy backpack with space for all your essentials, made from durable Tarpaulin with adjustable leather straps."
+        img={Banner1}
+      />
     </>
   );
 }
