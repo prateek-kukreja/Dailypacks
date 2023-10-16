34f9f60584e278f09aa36cd8fb934d7f0a239b5a
Prateek <prateekkukreja@outlook.com>
2023-10-16 23:21:35 +0530

    Extend Tailwind CSS configuration for custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color: "rgba(0, 0, 0, 0.205)",
+        hover: "rgba(0, 0, 0, .7)",
+      },
+    },
   },
   plugins: [],
 };
