ce9226aa7e2cf79538bd794e3f0bf55543cc2e80
Prateek <prateekkukreja@outlook.com>
2024-09-25 18:47:08 +0530

    remove alert message

--- client/src/hook/useSignin.jsx
+++ client/src/hook/useSignin.jsx
@@ -1,51 +1,44 @@
-import { useState } from "react";
-import { useAuthContext } from "../hook/useAuthContext";
+import { useState } from 'react'
+import { useAuthContext } from '../hook/useAuthContext'
 
 export const useSignin = () => {
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-  const { dispatch } = useAuthContext();
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+  const { dispatch } = useAuthContext()
 
   const signin = async (email, password) => {
-    setIsLoading(true);
-    setError(null);
-
-    const interruptionMessage = setTimeout(() => {
-      alert(
-        "Please note that our backend deployment process may experience temporary interruptions due to inactivity detection by our hosting provider. If such an interruption occurs, deployment will automatically resume after approximately 50 seconds. We apologize for any inconvenience this may cause and appreciate your understanding."
-      );
-    }, 5000);
+    setIsLoading(true)
+    setError(null)
 
     const response = await fetch(
-      "https://dailypacks.onrender.com/api/auth/login",
+      'https://dailypacks.onrender.com/api/auth/login',
       {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
       }
-    );
-    const res_data = await response.json();
+    )
+    const res_data = await response.json()
 
     if (!response.ok) {
-      clearTimeout(interruptionMessage);
-      setIsLoading(false);
-      setError(res_data.error);
-      return false; // return false on failure
+      setIsLoading(false)
+      setError(res_data.error)
+      return false // return false on failure
     }
 
     if (response.ok) {
-      clearTimeout(interruptionMessage);
+      clearTimeout(interruptionMessage)
 
       // save the user to local storage
-      localStorage.setItem("user", JSON.stringify(res_data));
+      localStorage.setItem('user', JSON.stringify(res_data))
 
       // update the auth context
-      dispatch({ type: "LOGIN", payload: res_data });
+      dispatch({ type: 'LOGIN', payload: res_data })
 
-      setIsLoading(false);
-      return true; // return true on success
+      setIsLoading(false)
+      return true // return true on success
     }
-  };
+  }
 
-  return { signin, error, isLoading };
-};
+  return { signin, error, isLoading }
+}
