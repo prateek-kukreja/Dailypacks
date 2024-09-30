75cba0a0edc396cc412be1bb7b82449b468853ef
Prateek <prateekkukreja@outlook.com>
2024-09-30 23:33:18 +0530

    refactor marquee

--- client/src/components/ServerDelayMessageBanner.jsx
+++ client/src/components/ServerDelayMessageBanner.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function ServerDelayMessageBanner() {
   return (
-    <div className="w-full border border-[#e7195a] text-[#e7195a] rounded p-2.5 border-solid bg-[#ffefef] overflow-hidden ">
-      <div class="flex flex-shrink-0 whitespace-nowrap animate-scroll">
-        <p class="inline-block">
+    <marquee
+      behavior="scroll"
+      direction="left"
+      className="fixed top-16 w-full border border-[#e7195a] text-[#e7195a] rounded p-2.5 border-solid bg-[#ffefef] overflow-hidden -rotate-3"
+    >
+      <div class="flex space-x-2 ">
+        <p class="inline-block animate-scroll">
+          Please note that our backend deployment process may experience
+          temporary interruptions due to inactivity detection by our hosting
+          provider. If such an interruption occurs, deployment will
+          automatically resume after approximately 50 seconds. We apologize for
+          any inconvenience this may cause and appreciate your understanding.
+        </p>
+        <p class="inline-block animate-scroll">
           Please note that our backend deployment process may experience
           temporary interruptions due to inactivity detection by our hosting
           provider. If such an interruption occurs, deployment will
@@ -12,7 +23,7 @@ function ServerDelayMessageBanner() {
           any inconvenience this may cause and appreciate your understanding.
         </p>
       </div>
-    </div>
+    </marquee>
   )
 }
 
